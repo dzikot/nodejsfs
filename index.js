2a24052dc39fd9734930abeb24782aa56c818616
dzikot <74687284+dzikot@users.noreply.github.com>
2020-12-05 23:16:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import createReadStream from 'fs';
+import fs from 'fs';
 import crypto from 'crypto';
 import http from 'http';
 
 import appSrc from './app.js';
-const app = appSrc(express, bodyParser, createReadStream, crypto, http);
+const app = appSrc(express, bodyParser, fs.createReadStream, crypto, http);
 
 app.listen(4321);
